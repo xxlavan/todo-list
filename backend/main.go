a3299864d596fc7c79d09c022c44000d508f1364
eoskadi <xxlavan@gmail.com>
2026-02-25 16:32:39 +0300

    change to golang

---
+++ backend/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	_ "github.com/lib/pq"
+)
+
+type Todo struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+var db *sql.DB
+
+func main() {
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		dsn = "postgres://postgres:postgres@db:5432/todo?sslmode=disable"
+	}
+
+	var err error
+	db, err = sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	createTable()
+
+	http.HandleFunc("/todos", todosHandler)
+	http.HandleFunc("/todos/", deleteHandler)
+
+	log.Println("Server running on :8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func createTable() {
+	query := `
+	CREATE TABLE IF NOT EXISTS todos (
+		id SERIAL PRIMARY KEY,
+		title TEXT NOT NULL,
+		completed BOOLEAN DEFAULT FALSE
+	);`
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func todosHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getTodos(w)
+	case http.MethodPost:
+		createTodo(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func getTodos(w http.ResponseWriter) {
+	rows, err := db.Query("SELECT id, title, completed FROM todos")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	defer rows.Close()
+
+	var todos []Todo
+	for rows.Next() {
+		var t Todo
+		rows.Scan(&t.ID, &t.Title, &t.Completed)
+		todos = append(todos, t)
+	}
+
+	json.NewEncoder(w).Encode(todos)
+}
+
+func createTodo(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Query().Get("title")
+	if title == "" {
+		http.Error(w, "title required", 400)
+		return
+	}
+
+	var id int
+	err := db.QueryRow(
+		"INSERT INTO todos(title) VALUES($1) RETURNING id",
+		title,
+	).Scan(&id)
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]int{"id": id})
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/todos/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "invalid id", 400)
+		return
+	}
+
+	_, err = db.Exec("DELETE FROM todos WHERE id=$1", id)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
